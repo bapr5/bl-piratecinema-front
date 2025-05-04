848202e6cd3dfbb9bb72f72cca4f1b3191c63c4f
bourne again pr5 <137082411+bapr5@users.noreply.github.com>
2025-05-04 18:13:44 +0300

    fix for apiPath

--- app/src/utils/utils.mjs
+++ app/src/utils/utils.mjs
@@ -1,5 +1,5 @@
 import { API_ADDR, API_PORT } from "../api/userAPI.mjs";
 
 export function makeUrl(path) {
-    return `http://${API_ADDR}:${API_PORT}${path}`;
+    return `https://${API_ADDR}${path}`;
 }
